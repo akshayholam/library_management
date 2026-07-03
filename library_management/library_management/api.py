acf5d2f16993ceafd2b9e33424bb73d77e630d1a
akshayholam <akshayholam3738@gmail.com>
2026-07-03 11:07:03 +0000

    Added weather and joke callout APIs

--- library_management/library_management/api.py
+++ library_management/library_management/api.py
@@ -14,3 +14,30 @@ def get_articles():
 def get_article(name):
     article = frappe.get_doc('Article', name)
     return article
+@frappe.whitelist()
+def get_random_joke():
+    import requests
+    response = requests.get('https://official-joke-api.appspot.com/random_joke')
+    if response.status_code == 200:
+        joke = response.json()
+        return {
+            'setup': joke['setup'],
+            'punchline': joke['punchline']
+        }
+    else:
+        frappe.throw('Failed to fetch joke!')
+@frappe.whitelist()
+def get_weather():
+    import requests
+    response = requests.get('https://wttr.in/Pune?format=j1')
+    if response.status_code == 200:
+        data = response.json()
+        temp = data['current_condition'][0]['temp_C']
+        weather = data['current_condition'][0]['weatherDesc'][0]['value']
+        return {
+            'city': 'Pune',
+            'temperature': f'{temp}°C',
+            'condition': weather
+        }
+    else:
+        frappe.throw('Failed to fetch weather!')
