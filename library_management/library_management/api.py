2ce7a47dbb75067305ff3bb1517aa0b6ed8997f7
akshayholam <akshayholam3738@gmail.com>
2026-07-03 10:55:12 +0000

    Added Custom APIs - get_articles and get_article

--- library_management/library_management/api.py
+++ library_management/library_management/api.py
@@ -2,3 +2,15 @@ import frappe
 
 def send_notification(doc, method):
     frappe.msgprint(f'Hook triggered! Article "{doc.article_name}" was saved!')
+
+@frappe.whitelist()
+def get_articles():
+    articles = frappe.get_list('Article',
+        fields=['name', 'article_name', 'author', 'status'],
+        order_by='creation desc'
+    )
+    return articles
+@frappe.whitelist()
+def get_article(name):
+    article = frappe.get_doc('Article', name)
+    return article
